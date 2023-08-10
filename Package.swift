6a936eaf684ae441a4886fdc2eda6a469c3550b0
Quico Moya <me@qmoya.com>
2023-08-10 22:45:56 +0200

    Add documentation workflow

--- Package.swift
+++ Package.swift
@@ -14,6 +14,9 @@ let package = Package(
       targets: ["FeatureToggles"]
     )
   ],
+  dependencies: [
+	.package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
+  ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
